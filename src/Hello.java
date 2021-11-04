d51e0c1916dd3ec17dff83cf02a0bcae964c1efb
digimaktab <alighaffari70@gmail.com>
2021-11-04 19:51:19 +0330

    add summer

--- src/Hello.java
+++ src/Hello.java
@@ -4,6 +4,11 @@ public class Hello {
     // test
     public static void main(String[] args) {
         System.out.println("Hello World 🌏");
+        System.out.println(summer(1,2));
+    }
+
+    public static int summer(int a, int b) {
+        return a+b;
     }
 
 }
